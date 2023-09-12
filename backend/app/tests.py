8387ed90b4fafde9ed6924a11a06b92f9b68ddcf
mikefromru <googlockgood@gmail.com>
2023-09-12 20:49:21 +0600

    Added test_delete_level

--- backend/app/tests.py
+++ backend/app/tests.py
@@ -7,10 +7,10 @@ class SearchTest(APITestCase):
 
     def setUp(self):
         self.client = APIClient()
-        self.car = Level.objects.create(id=1, name='About car')
-        self.crime = Level.objects.create(id=2, name='About crime')
-        self.politics = Level.objects.create(id=3, name='About politics')
-        self.travel = Level.objects.create(id=4, name='travel')
+        Level.objects.create(id=1, name='About car')
+        Level.objects.create(id=2, name='About crime')
+        Level.objects.create(id=3, name='About politics')
+        Level.objects.create(id=4, name='travel')
 
     def test_get_index_page(self):
         response = self.client.get('')
@@ -28,6 +28,11 @@ class SearchTest(APITestCase):
         response_with_empty_list = self.client.get('http://localhost:8000/api/app/search/?search=kakeknfkeke')
         self.assertEqual(len(response_with_empty_list.data), 0)
     
-    def test_all(self):
+    def test_all_url(self):
         response = self.client.get('http://localhost:8000/api/app/all/')
         self.assertEqual(response.status_code, 200)
+
+    def test_delete_level(self):
+        id_3 = Level.objects.get(id=3).delete()
+        lst = Level.objects.all().count()
+        self.assertEqual(lst, 3)
