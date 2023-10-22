077cf1ff37b42dcdd50bc0f88a969856a54a0232
mikefromru <googlockgood@gmail.com>
2023-10-22 22:16:43 +0300

    Split settings

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -25,14 +25,20 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+   path('admin/', admin.site.urls),
 
-    path('api/app/', include(api_urlpatterns)),
-    path('', include('app.urls')),
+   path('api/app/', include(api_urlpatterns)),
+   path('', include('app.urls')),
 
-    re_path(r'swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+   re_path(r'swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   re_path(r'swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   re_path(r'redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
+
+if settings.DEBUG:
+   import debug_toolbar
+   urlpatterns = [
+      path('__debug__/', include(debug_toolbar.urls)),
+   ] + urlpatterns
