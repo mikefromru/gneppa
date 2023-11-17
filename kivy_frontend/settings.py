3f1254757a7dba6d42f4fe6e084c6bce8885fb59
mikefromru <googlockgood@gmail.com>
2023-11-17 23:56:44 +0300

    settings file for kivy frontend app

---
+++ kivy_frontend/settings.py
@@ -0,0 +1,16 @@
+from kivy.utils import platform
+from kivy.core.window import Window
+from settings_local import *
+
+if platform == 'linux':
+    Window.size = (420, 820)
+    Window.top = 10
+    Window.left 
+    url = URL
+else:
+    url = URL
+
+whatsapp_link = WHATSAP_LINK
+telegram_link = TELEGRAM_LINK
+
+
